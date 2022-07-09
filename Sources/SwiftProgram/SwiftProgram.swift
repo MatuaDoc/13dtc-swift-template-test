79979af5eb5534a6891889d5e9a433ede6856410
Matua Doc <88599935+MatuaDoc@users.noreply.github.com>
2022-07-09 14:40:29 +1200

    Initial commit

---
+++ Sources/SwiftProgram/SwiftProgram.swift
@@ -0,0 +1,16 @@
+
+//
+//  main.swift
+//  SwiftProgram
+//
+//  Created by Student Name on 1/01/23.
+//
+
+@main
+public struct SwiftProgram {
+    public private(set) var text = "Hello, world!"
+
+    public static func main() {
+        print(SwiftProgram().text)
+    }
+}
